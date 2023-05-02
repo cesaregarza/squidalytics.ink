76f70762c1295115dfc584a6048b7061dc510de5
Cesar E Garza <cesar@cegarza.com>
2023-05-02 02:23:58 -0500

    Woops minor error

--- squidalytics/squidalytics/settings.py
+++ squidalytics/squidalytics/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
