af9c8b3fe5d02b1ff0097a1222c7cccb4b53ca22
Cesar E Garza <cesar@cegarza.com>
2023-05-02 03:12:40 -0500

    changes to views

--- squidalytics/landing/views.py
+++ squidalytics/landing/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
 
 # Create your views here.
 def hello_world(request: HttpRequest) -> HttpResponse:
-    return render(request, 'landing/hello_world.html')
+    return render(request, "landing/hello_world.html")
