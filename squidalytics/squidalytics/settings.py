0db00b99dddf7467951ffc1c15f451d63d4615ac
Cesar E Garza <cesar@cegarza.com>
2023-05-01 22:40:09 -0500

    Debug set to false

--- squidalytics/squidalytics/settings.py
+++ squidalytics/squidalytics/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 if DEBUG:
     # Set the secret key to a known value for development
