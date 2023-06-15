0beb60a4ffa03be20afefe15d603611c39b64500
Cesar E Garza <cesar@cegarza.com>
2023-06-15 00:59:16 -0500

    Added modals for images

---
+++ squidalytics/static/js/modal.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('.open-modal-button').forEach(function(el) {
+    el.addEventListener('click', function() {
+        this.nextElementSibling.classList.add('is-active');
+    });
+});
+
+document.querySelectorAll('.modal-close').forEach(function(el) {
+    el.addEventListener('click', function() {
+        this.parentElement.classList.remove('is-active');
+    });
+});
