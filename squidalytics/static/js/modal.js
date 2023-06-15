5be268dedbc5635a3d8abb260690bac33ff25a5b
Cesar E Garza <cesar@cegarza.com>
2023-06-15 17:39:35 -0500

    Updated modal to go away when clicking on the background

--- squidalytics/static/js/modal.js
+++ squidalytics/static/js/modal.js
@@ -9,3 +9,9 @@ document.querySelectorAll('.modal-close').forEach(function(el) {
         this.parentElement.classList.remove('is-active');
     });
 });
+
+document.querySelectorAll('.modal-background').forEach(function(el) {
+    el.addEventListener('click', function() {
+        this.parentElement.classList.remove('is-active');
+    });
+});
